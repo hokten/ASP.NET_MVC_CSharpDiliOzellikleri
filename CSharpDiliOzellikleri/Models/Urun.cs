59ba4a9ac0de14a79b1ddc99496da1fd3a8a6df1
hokten <hokten@gmail.com>
2018-03-06 14:51:34 +0200

    Urun modeli düzenlendi.

--- CSharpDiliOzellikleri/Models/Urun.cs
+++ CSharpDiliOzellikleri/Models/Urun.cs
@@ -7,11 +7,25 @@ namespace CSharpDiliOzellikleri.Models
 {
     public class Urun
     {
+        private int urunID;
         private string adi;
+        private string aciklama;
+        private decimal fiyat;
+        private string kategori;
+        public int UrunID
+        {
+            get { return urunID; }
+            set { urunID = value; }
+        }
         public string Adi
         {
             get { return adi; }
             set { adi = value; }
+        }
+        public string Aciklama
+        {
+            get { return aciklama; }
+            set { aciklama = value; }
         }
     }
 }
