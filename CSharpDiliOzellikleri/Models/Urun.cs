7b074577162a2683eca1fd5672c8ae49a142bfe2
hokten <hokten@gmail.com>
2018-03-06 14:38:19 +0200

    Urun modeli eklendi.

---
+++ CSharpDiliOzellikleri/Models/Urun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharpDiliOzellikleri.Models
+{
+    public class Urun
+    {
+        private string adi;
+        public string Adi
+        {
+            get { return adi; }
+            set { adi = value; }
+        }
+    }
+}
