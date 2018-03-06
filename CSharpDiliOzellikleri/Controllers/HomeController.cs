6d5b3333b3ff51584fe0ea841102abc6fda97177
hokten <hokten@gmail.com>
2018-03-06 15:27:37 +0200

    Sepetteki ürünlerin toplam fiyatını hesaplayan UzantiMetodlariKullan action metodu eklendi.

--- CSharpDiliOzellikleri/Controllers/HomeController.cs
+++ CSharpDiliOzellikleri/Controllers/HomeController.cs
@@ -52,5 +52,20 @@ namespace CSharpDiliOzellikleri.Controllers
             };
             return View("Result", (object)meyveDizisi[1]);
         }
+        public ViewResult UzantiMetodlariKullan()
+        {
+            Sepet sepetim = new Sepet
+            {
+                Urunler = new List<Urun>
+                {
+                    new Urun {Adi="Kayık", Fiyat=175M },
+                    new Urun {Adi="Can Yeleği", Fiyat=35M },
+                    new Urun {Adi="Futbol Topu", Fiyat=15M },
+                    new Urun {Adi="Krampon", Fiyat=45M}
+                }
+            };
+            decimal sepetYekunu = sepetim.ToplamFiyat();
+            return View("Result", (object)String.Format("Toplam Sepet Fiyatı : {0:c}", sepetYekunu));
+        }
     }
 }
