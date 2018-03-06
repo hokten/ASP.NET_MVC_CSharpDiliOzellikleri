e5a0ecdff82efc7950008806ce376b09ca3b7e19
hokten <hokten@gmail.com>
2018-03-06 14:57:24 +0200

    Adi özelliğinin çekim değererine UrunID eklendi.

--- CSharpDiliOzellikleri/Models/Urun.cs
+++ CSharpDiliOzellikleri/Models/Urun.cs
@@ -7,8 +7,20 @@ namespace CSharpDiliOzellikleri.Models
 {
     public class Urun
     {
+        private string adi;
+
         public int UrunID { get; set; }
-        public string Adi { get; set; }
+        public string Adi
+        {
+            get
+            {
+                return UrunID + adi;
+            }
+            set
+            {
+                adi = value;
+            }
+        }
         public string Aciklama { get; set; }
         public decimal Fiyat { get; set; }
         public string Kategori { set; get; }
