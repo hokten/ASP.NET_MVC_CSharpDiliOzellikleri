91d0e492c04a4ab7c1a3467fa06bbe6adba7eb4c
hokten <hokten@gmail.com>
2018-03-06 14:45:09 +0200

    OtoOzellik action metodu eklendi.

--- CSharpDiliOzellikleri/Controllers/HomeController.cs
+++ CSharpDiliOzellikleri/Controllers/HomeController.cs
@@ -12,7 +12,19 @@ namespace CSharpDiliOzellikleri.Controllers
         // GET: Home
         public string Index()
         {
-            return "Deneme istediğini URL'yi açın";
+            return "Deneme istediğiniz URL'yi açın";
+        }
+        public ViewResult OtoOzellik()
+        {
+            // yeni bir urun nesnesi olustur
+            Urun urunum = new Urun();
+            // urunum nesnesinin Adi özelliğine bir değer ata
+            urunum.Adi = "Kayık";
+            // urunum nesnesinin Adi özelliğindeki değeri çek
+            string urunIsmi = urunum.Adi;
+            // view üret
+            return View("Result",
+            (object)String.Format("Ürün adı: {0}", urunIsmi));
         }
     }
 }
