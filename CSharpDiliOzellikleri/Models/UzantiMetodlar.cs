f7b368f98afbf01090a7778f241142523768f935
hokten <hokten@gmail.com>
2018-03-06 15:20:08 +0200

    Bir veya birden fazla Urun nesnesinden oluşan Sepet modeli ve Sınıfı dışından sınıfa metod ekleme örneği olarak UzantiMetodlar modeli oluşturuldu.

---
+++ CSharpDiliOzellikleri/Models/UzantiMetodlar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSharpDiliOzellikleri.Models
+{
+    public static class UzantiMetodlar
+    {
+        public static decimal ToplamFiyat(this Sepet sepetim)
+        {
+            decimal yekun = 0;
+            foreach (Urun urn in sepetim.Urunler)
+            {
+                yekun += urn.Fiyat;
+            }
+            return yekun;
+        }
+    }
+}
