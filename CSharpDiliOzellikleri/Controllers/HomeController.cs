d6c52bb6b783bdaeadab78c10e2b36347f05a33c
hokten <hokten@gmail.com>
2018-03-06 14:33:18 +0200

    HomeController.cs dosyasının Index action metodu düzenlendi.

--- CSharpDiliOzellikleri/Controllers/HomeController.cs
+++ CSharpDiliOzellikleri/Controllers/HomeController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CSharpDiliOzellikleri.Models;
 
 namespace CSharpDiliOzellikleri.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return "Deneme istediğini URL'yi açın";
         }
     }
 }
